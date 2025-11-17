ab049b57b857716dc432f8bd53f96532e3bfca5a
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-11-17 12:34:03 +0100

    Update files
    from uup sta app version: 1.8.2-dev

--- shared/main.php
+++ shared/main.php
@@ -1,14 +1,14 @@
 <?php
 // Website information
-$websiteVersion = '1.8.1';
-$requiredApi = '1.44.0';
+$websiteVersion = '1.8.2-dev';
+// $requiredApi = '1.44.0';
 
 require_once dirname(__FILE__).'/../api/shared/main.php';
 require_once dirname(__FILE__).'/utils.php';
 require_once dirname(__FILE__).'/lang.php';
 
 // Do check of API
-checkApi();
+// checkApi();
 
 $s['uupdump'] = 'UUP dump app';
 $s['uupdumpSub'] = '%s - UUP dump app';
--- sta/fetchupd.php
+++ sta/fetchupd.php
@@ -6,12 +6,28 @@ $build = isset($argv[4]) ? $argv[4] : 16251;
 $minor = isset($argv[5]) ? intval($argv[5]) : 0;
 $sku = isset($argv[6]) ? intval($argv[6]) : 48;
 $type = isset($argv[7]) ? $argv[7] : 'Production';
+$branch = isset($argv[8]) ? $argv[8] : 'auto';
 
 require_once dirname(__FILE__).'/../api/fetchupd.php';
 require_once dirname(__FILE__).'/main.php';
 
 consoleLogger(brand('fetchupd'));
-$fetchedUpdate = uupFetchUpd($arch, $ring, $flight, $build, $minor, $sku, $type);
+
+[$build, $flags] = uupApiPrivateParseFlags($build);
+$params = [
+    'arch' => $arch,
+    'ring' => $ring,
+    'flight' => $flight,
+    'branch' => $branch,
+    'build' => $build,
+    'minor' => $minor,
+    'sku' => $sku,
+    'type' => $type,
+    'flags' => $flags,
+];
+$fetchedUpdate = uupFetchUpd2($params, 0);
+
+// $fetchedUpdate = uupFetchUpd($arch, $ring, $flight, $build, $minor, $sku, $type);
 if(isset($fetchedUpdate['error'])) {
     throwError($fetchedUpdate['error']);
 }
